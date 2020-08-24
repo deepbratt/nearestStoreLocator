a8ee5ae85d88086880da78e9ad71fa342a1ddd22
Deep Bratt Mondal <deepbratt@gmail.com>
2020-08-25 01:25:30 +0530

    sadaa

--- routes/location.js
+++ routes/location.js
@@ -3,7 +3,18 @@ const mongoose = require("mongoose");
 const Location = mongoose.model("Location");
 
 router.get("/get", async (req, res) => {
-  const findData = await Location.find({});
+  const findData = await Location.find({
+    geometry: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: [req.body.lat, req.body.long],
+        },
+        $maxDistance: 100,
+        $minDistance: 10,
+      },
+    },
+  });
   res.send(findData);
 });
 
